8c89632db318ff6bea7b651850d05ab9d957eb94
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-05-26 13:31:09 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email){
+    const configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    }
+
+    return fetch('http://localhost:3000/users', configObj)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            const body = document.querySelector('body');
+            const newContent = document.createElement('h1');
+            newContent.textContent = data.id;
+            body.append(newContent);
+        })
+        .catch(function (error) {
+            const body = document.querySelector('body');
+            const newContent = document.createElement('h1');
+            newContent.textContent = error.message;
+            body.append(newContent);
+        })
+}
